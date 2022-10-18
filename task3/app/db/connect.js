c348da2fd7e1bd4f0e5f0ccc8ec723af756e797f
kerollosMohsen <115957504+kerollosMohsen@users.noreply.github.com>
2022-10-18 02:16:32 +0200

    Delete task3 directory

--- task3/app/db/connect.js
+++
@@ -1,9 +0,0 @@
-const MonogoClient = require("mongodb").MongoClient
-const myConnection = (cb)=>{
-    MonogoClient.connect(process.env.DBURL, (err, client)=>{
-        if(err) return cb(err, null)
-        const db = client.db(process.env.DBNAME)
-        cb(null, db)
-    })
-}
-module.exports = myConnection
